aac70163dd30bfeb257f560578e5ba9f93fd6753
yecheng.123 <yecheng.123@bytedance.com>
2022-09-17 16:20:46 +0800

    info

--- container_num/src/main/java/com/yecheng/container_num/container_num/Util/Commands.java
+++ container_num/src/main/java/com/yecheng/container_num/container_num/Util/Commands.java
@@ -4,20 +4,20 @@ public class Commands {
     
     public static String[] GetServiceNums(String service) {
         return new String[] {
-            "/bin/bash", "-c", "docker ps --format \"{{.Image}}\" | grep " + service + " | wc -l",
+            "/bin/sh", "-c", "docker ps --format \"{{.Image}}\" | grep " + service + " | wc -l",
         };
     }
 
     public static String[] JudgePortAvaliable(int port) {
         return new String[] {
-            "/bin/bash", "-c", "netstat -anp | grep " + port + " | wc -l" 
+            "/bin/sh", "-c", "netstat -anp | grep " + port + " | wc -l" 
         };
     }
 
     public static String[] RunContainer(String service, int port) {
         String name = service.replace('/', '_');
         return new String[] { 
-            "/bin/bash",
+            "/bin/sh",
             "-c",
             String.format("docker run -d -p %d:%d --name=%s_%d %s java -jar app.jar " +
             "--server.port=%d --spring.cloud.consul.discovery.instance-id=%s_%d ",
