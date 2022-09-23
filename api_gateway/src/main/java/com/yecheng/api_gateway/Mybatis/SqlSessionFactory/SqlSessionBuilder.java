73ba3ca9669e5303b16b2691be294d63d3ac150a
yecheng.123 <yecheng.123@bytedance.com>
2022-09-23 15:49:47 +0800

    info

--- api_gateway/src/main/java/com/yecheng/api_gateway/Mybatis/SqlSessionFactory/SqlSessionBuilder.java
+++ api_gateway/src/main/java/com/yecheng/api_gateway/Mybatis/SqlSessionFactory/SqlSessionBuilder.java
@@ -2,6 +2,8 @@ package com.yecheng.api_gateway.Mybatis.SqlSessionFactory;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -12,7 +14,8 @@ import com.yecheng.ymysql.DataSource.SqlSessionPool;
 
 @Component
 public class SqlSessionBuilder implements CommandLineRunner{
-    
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(SqlSessionBuilder.class);
+
     @Autowired
     private DiscoveryClient client;
     private SqlSessionPool sqlSessionPool;
@@ -21,6 +24,7 @@ public class SqlSessionBuilder implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
+        logger.info("begin init mysql source");
         sqlSessionPool = new SqlSessionPool("mysql", "myDatas", "root", "123456yd", client);
         sqlSessionPool.initSqlSessionPool();
         
