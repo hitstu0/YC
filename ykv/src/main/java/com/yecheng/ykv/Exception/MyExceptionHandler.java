cb00b650858a8e08dcfb286b7521a74db16bb12c
yecheng.123 <yecheng.123@bytedance.com>
2022-09-23 21:44:09 +0800

    info

---
+++ ykv/src/main/java/com/yecheng/ykv/Exception/MyExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.yecheng.ykv.Exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yecheng.ykv.Data.CodeMsg;
+
+@ControllerAdvice
+public class MyExceptionHandler {
+    
+    @ExceptionHandler(value = MyException.class)
+    @ResponseBody
+    public CodeMsg handel(MyException e) {
+        return e.getCodeMsg();
+    }
+}
