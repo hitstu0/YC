cad071db2d7cd069ca53b5b25a78d4e8e1e8b877
yecheng.123 <yecheng.123@bytedance.com>
2022-10-05 11:59:43 +0800

    info

--- log_manager/src/main/java/com/yecheng/log_manager/Data/Consumer.java
+++ log_manager/src/main/java/com/yecheng/log_manager/Data/Consumer.java
@@ -26,7 +26,7 @@ public class Consumer {
 
     @Bean(name = "rmqconsumer")
     public DefaultMQPushConsumer getConsumer() throws MQClientException {
-        ConsumerBuilder builder = new ConsumerBuilder(discoveryClient, "namesrv");
+        ConsumerBuilder builder = new ConsumerBuilder(discoveryClient, "rocketmq");
         try {
             DefaultMQPushConsumer consumer = builder.getConsumer("yecheng", "logs", "send", new Listener());
             consumer.start();
