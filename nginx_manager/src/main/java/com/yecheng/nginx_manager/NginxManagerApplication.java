030fa383ee350ec0f918ee7f9455aa40c462a7fa
yecheng.123 <yecheng.123@bytedance.com>
2022-10-24 21:12:05 +0800

    info

--- nginx_manager/src/main/java/com/yecheng/nginx_manager/NginxManagerApplication.java
+++ nginx_manager/src/main/java/com/yecheng/nginx_manager/NginxManagerApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class NginxManagerApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("rocketmq.client.logRoot","lo/");
+		
 		SpringApplication.run(NginxManagerApplication.class, args);
 	}
 
