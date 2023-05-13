33d396ca9e2fab3bddf32581f960cb6e219d58a4
yecheng <2064355949@qq.com>
2023-05-13 21:14:18 +0800

    info

---
+++ rsa_server/src/main/java/com/yecheng/rsa_server/RsaServerApplication.java
@@ -0,0 +1,13 @@
+package com.yecheng.rsa_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsaServerApplication.class, args);
+	}
+
+}
