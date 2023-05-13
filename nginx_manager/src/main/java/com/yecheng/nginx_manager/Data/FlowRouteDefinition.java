9c6d1b0cc7ba4cda574f9aa08c9e8ef681e10c45
yecheng <2064355949@qq.com>
2023-05-13 20:55:03 +0800

    info

--- nginx_manager/src/main/java/com/yecheng/nginx_manager/Data/FlowRouteDefinition.java
+++
@@ -1,9 +0,0 @@
-package com.yecheng.nginx_manager.Data;
-
-import lombok.Data;
-
-@Data
-public class FlowRouteDefinition {
-    private String host;
-    private String service;
-}
--- yredis/src/main/java/com/yecheng/yredis/Util/Prefix/prefix.java
+++
@@ -1,6 +0,0 @@
-package com.yecheng.yredis.Util.Prefix;
-
-public interface prefix {
-    int getExpiredTime();
-    String getPrefix();
-}
