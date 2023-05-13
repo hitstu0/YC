20850994e2721c6b72f66b944b20e746ebcee9fd
yecheng <2064355949@qq.com>
2023-05-13 21:06:54 +0800

    info

--- api_manager/src/main/java/com/yecheng/api_manager/Data/RouteDefinitionData.java
+++
@@ -1,9 +0,0 @@
-package com.yecheng.api_manager.Data;
-
-import lombok.Data;
-
-@Data
-public class RouteDefinitionData {
-    private int id;
-    private String path;
-}
--- nginx_manager/src/main/java/com/yecheng/nginx_manager/Data/RouteDefinitionData.java
+++
@@ -1,9 +0,0 @@
-package com.yecheng.nginx_manager.Data;
-
-import lombok.Data;
-
-@Data
-public class RouteDefinitionData {
-    private int id;
-    private String path;
-}
